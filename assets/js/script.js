826894e825ed2ced0977318c7503b09cd0c49a9f
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-31 19:26:40 +1000

    current date/time added with moment js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 // Display current time and day at top of page
 
-var currentDayEl = $("#currentDay");
+// var currentDayEl = $("#currentDay");
 
-currentDayEl.text('hello, test');
+// currentDayEl.text(moment().format('LLL');)
 
 // Check state of page?
+
+var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").text(time);
