2793b506967a893d0c541c6517543e60a25d2368
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-31 20:23:00 +1000

    document.ready funtion added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,47 @@
-// Displays current date and time at top of page
+$(document).ready(function () {
 
-var currentDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").text(currentDateTime);
+    // Displays current date and time at top of page
 
-// add event listener for save icons
+    var currentDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(currentDateTime);
 
-$('.saveBtn').on('click', function () {
-    // get values
-    var value = $(this).siblings('.description').val();
-    var time = $(this).parent().attr('id');
+    // add event listener for save icons
 
-    // save to localStorage
+    $('.saveBtn').on('click', function () {
+        // get values
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
 
-    localStorage.setItem(time, value);
+        // save to localStorage
+
+        localStorage.setItem(time, value);
+
+    });
+
+    function hourUpdater() {
+        // get current number of hours
+        var currentHour = moment().hours();
+
+
+        // loop over time blocks
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+            // check if time has passed time of time block
+
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    hourUpdater();
 
 });
-
-function hourUpdater() {
-    var currentHour = moment().hours();
-
-
-    // loop over time blocks
-    $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
-
-        // check if time has passed time of time block
-
-        if (blockHour < currentHour) {
-            $(this).addClass('past');
-        } else if (blockHour === currentHour) {
-            $(this).removeClass('past');
-            $(this).addClass('present');
-        } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass('future');
-        }
-    });
-}
-
-hourUpdater();
